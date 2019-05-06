b0a3166d1dced9cf365af0cf05de619504ad88ea
SlickBot <slickeeey@gmail.com>
2019-05-06 23:08:19 +0200

    Added FakeAnalyze to analyze device performance while testing

--- src/main/kotlin/com/ulj/slicky/appiumcompare/FakeSocialTester.kt
+++ src/main/kotlin/com/ulj/slicky/appiumcompare/FakeSocialTester.kt
@@ -9,41 +9,47 @@ import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
+import java.io.File
 import java.net.URL
+import kotlin.concurrent.thread
 
 /**
  * Created by SlickyPC on 12.6.2017
  */
-class FakeSocialTester {
 
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val tester = FakeSocialTester()
-            listOf(
-                    "JavaFakeSocial",
-                    "KotlinFakeSocial",
-                    "AnkoFakeSocial"
-            ).forEach { tester.testProject(it) }
+fun main() {
+    val tester = FakeSocialTester()
+
+    val projects = listOf(
+            "JavaFakeSocial",
+            "KotlinFakeSocial",
+            "AnkoFakeSocial"
+    )
+
+    repeat(30) {
+        projects.forEach {
+            tester.testProject(it)
         }
     }
+}
+
+class FakeSocialTester {
 
     fun testProject(projectName: String) {
-
-        println()
-        printf("Starting $projectName")
+        printf("\nStarting $projectName")
 
         val capabilities = DesiredCapabilities().apply {
-            setCapability(DEVICE_NAME, "emulator-5554")
+//            setCapability(DEVICE_NAME, "emulator-5554")
+            setCapability(DEVICE_NAME, "21f985ccab0d7ece")
             setCapability(PLATFORM_NAME, "Android")
             setCapability(NEW_COMMAND_TIMEOUT, 120)
             setCapability(AUTOMATION_NAME, "uiautomator2")
-//            setCapability(APPIUM_VERSION, "1.7.2")
+            setCapability("autoGrantPermissions", true)
+            setCapability(APPIUM_VERSION, "1.7.2")
 //            setCapability(FULL_RESET, true)
-//            setCapability(AUTO_WEBVIEW, true)
-            setCapability(APP, javaClass.getResource("/$projectName/app-release.apk").path)
-            setCapability(APP_PACKAGE, "com.ulj.slicky.${projectName.toLowerCase()}")
-            setCapability(APP_ACTIVITY, ".activity.content.ContentActivity")
+            setCapability(APP, findApkPath(projectName))
+            setCapability(APP_PACKAGE, findAppPackage(projectName))
+            setCapability(APP_ACTIVITY, findActivityPackage(projectName))
 //            setCapability(APP_WAIT_PACKAGE, "com.ulj.slicky.${projectName.toLowerCase()}")
 //            setCapability(APP_WAIT_ACTIVITY, ".activity.login.LoginActivity")
         }
@@ -52,15 +58,17 @@ class FakeSocialTester {
 
             println("DONE (sessionId: $sessionId)")
 
-            signUpRoutine()
-            signOutRoutine()
-            signInRoutine()
-            deleteContentRoutine()
-            createContentRoutine()
-            profileRoutine()
-            friendsRoutine()
-            settingsRoutine()
-            aboutRoutine()
+            analyze(projectName) {
+                signUpRoutine()
+                signOutRoutine()
+                signInRoutine()
+                deleteContentRoutine()
+                createContentRoutine()
+                profileRoutine()
+                friendsRoutine()
+                settingsRoutine()
+                aboutRoutine()
+            }
 
             println("$projectName finished!")
             closeApp()
@@ -68,17 +76,9 @@ class FakeSocialTester {
         }
     }
 
-    private fun printf(s: String, length: Int = 30) = print("%-${length}s".format(s))
-
-    private fun <T : WebElement> AndroidDriver<T>.waitTillVisible(by: By, timeoutSeconds: Long = 30): WebElement {
-        return WebDriverWait(this, timeoutSeconds).until(ExpectedConditions.visibilityOfElementLocated(by))
-    }
-
-    private fun <T : WebElement> AndroidDriver<T>.routine(name: String, op: AndroidDriver<T>.() -> Unit) {
-        printf("Running $name ")
-        op()
-        println("DONE [${currentActivity()}]")
-    }
+    /**
+     * ROUTINES
+     */
 
     private fun <T : WebElement> AndroidDriver<T>.signUpRoutine() = routine("signUpRoutine") {
         waitTillVisible(By.id("signin_signup_button")).click()
@@ -150,4 +150,140 @@ class FakeSocialTester {
         waitTillVisible(By.id("signin_signin_button")).click()
         waitTillVisible(By.id("content_item"))
     }
+
+    /**
+     * HELPERS
+     */
+
+    private fun findApkPath(projectName: String): String {
+        return javaClass.getResource("/$projectName/app-appium-proguard.apk").path
+    }
+
+    private fun findAppPackage(projectName: String): String {
+        return "com.ulj.slicky.${projectName.toLowerCase()}.appium"
+    }
+
+    private fun findActivityPackage(projectName: String): String {
+        return "com.ulj.slicky.${projectName.toLowerCase()}.activity.content.ContentActivity"
+    }
+
+    private fun <T : WebElement> AndroidDriver<T>.waitTillVisible(by: By, timeoutSeconds: Long = 30): WebElement {
+        return WebDriverWait(this, timeoutSeconds).until(ExpectedConditions.visibilityOfElementLocated(by))
+    }
+
+    private fun <T : WebElement> AndroidDriver<T>.routine(name: String, op: AndroidDriver<T>.() -> Unit) {
+        printf("Running $name ")
+        op()
+        println("DONE [${currentActivity()}]")
+    }
+
+    private fun printf(s: String, length: Int = 30) = print("%-${length}s".format(s))
+
+    private fun <T : WebElement> AndroidDriver<T>.analyze(projectName: String, function: (AndroidDriver<T>) -> Unit) {
+        val analyze = FakeAnalyzer()
+        var success = false
+
+        analyze.start()
+        try {
+            function(this)
+            success = true
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        analyze.stop()
+
+        if (success) {
+            val cpuFile = File("output-data", "$projectName-cpu-${System.currentTimeMillis()}.csv")
+            val memFile = File("output-data", "$projectName-mem-${System.currentTimeMillis()}.csv")
+            cpuFile.writer().use { it.write(analyze.cpuData.joinToString(",")) }
+            memFile.writer().use { it.write(analyze.memData.joinToString(",")) }
+        }
+    }
+
+    private class FakeAnalyzer {
+
+        private var shouldRun = false
+        private var thread: Thread? = null
+
+        val cpuData = mutableListOf<Double>()
+        val memData = mutableListOf<Double>()
+
+        fun start() {
+            if (shouldRun) return
+            shouldRun = true
+            thread = thread {
+                println("FakeAnalyzer thread started")
+                while (shouldRun) {
+                    try {
+                        startProcess()
+                    } catch (e: Exception) {
+                        if (shouldRun) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+                println("FakeAnalyzer thread stopped")
+            }
+        }
+
+        fun stop() {
+            shouldRun = false
+            thread?.interrupt()
+            thread = null
+        }
+
+        private fun startProcess() {
+            val adbPath = "${System.getProperty("user.home")}/Android/Sdk/platform-tools/adb"
+            val adbCommand = "shell top -n 1 -m 20"
+            val p = Runtime.getRuntime().exec("$adbPath $adbCommand")
+            p.waitFor()
+            p.inputStream.reader().use { reader ->
+                processLines(reader.readLines())
+            }
+        }
+
+        private fun processLines(lines: List<String>) {
+            val line = lines.firstOrNull { it.contains("com.ulj.slicky") }
+            var cpuUsage = -1.0
+            var memUsage = -1.0
+
+            if (line != null) {
+                try {
+                    val cpuMemSubstring = line.substring(42, 54).trim()
+                    val cpuMemSplit = cpuMemSubstring.split("\\s+".toRegex())
+                    cpuUsage = cpuMemSplit[0].toDouble()
+                    memUsage = cpuMemSplit[1].toDouble()
+                } catch (e: Exception) {
+                    println("Error for line: \"$line\"")
+                    e.printStackTrace()
+                }
+            }
+
+            cpuData += cpuUsage
+            memData += memUsage
+        }
+
+//        private fun startProcess() {
+//            val memoryInfo = getInfo("memoryinfo")
+//            memoryInfo.forEach { println(it) }
+//            val cpuInfo = getInfo("cpuinfo", 1000)
+//            cpuInfo.forEach { println(it) }
+//        }
+
+//        private fun getInfo(dataType: String, timeout: Int = 10): HashMap<String, Double> {
+//            val data = driver.getPerformanceData("com.ulj.slicky.${projectName.toLowerCase()}.appium", dataType, timeout)
+//            val readableData = HashMap<String, Double>()
+//            for (i in 0 until data[0].size) {
+//                val value = if (data[1][i] == null) {
+//                    0.0
+//                } else {
+//                    (data[1][i] as String).toDouble()
+//                }
+//                readableData[data[0][i] as String] = value
+//            }
+//            return readableData
+//        }
+
+    }
+
 }
